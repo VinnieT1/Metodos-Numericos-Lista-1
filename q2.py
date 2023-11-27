19734e771f01c0c2bed1d3bd36cda60b5f7c5cf4
Vinicius Teixeira <viniciusteixeirapereira2002@hotmail.com>
2023-11-27 08:37:34 -0300

    question 2 done

--- q2.py
+++ q2.py
@@ -1,3 +1,5 @@
+import math
+
 def abs_err(p, p_):
     return abs(p - p_)
 
@@ -5,8 +7,21 @@ def rel_err(p, p_):
     return abs((p - p_) / p)
 
 if __name__ == '__main__':
-    p = 1
-    p_ = 0.9
+    p = 124
+    p_ = 7
 
-    print('abs error:', p)
-    print('rel error:', p_)
+    print(f'abs error for p=1 and p*=0.9994:', abs_err(1, 0.9994))
+    print(f'rel error for p=1 and p*=0.9994:', rel_err(1, 0.9994))
+    print('')
+
+    print(f'abs error for p=124 and p*=7:', abs_err(124, 7))
+    print(f'rel error for p=124 and p*=7:', rel_err(124, 7))
+    print('')
+    
+    print(f'abs error for p=e^10 and p*=22000:', abs_err(math.exp(10), 22000))
+    print(f'rel error for p=e^10 and p*=22000:', rel_err(math.exp(10), 22000))
+    print('')
+    
+    print(f'abs error for p=8! and p*=39900:', abs_err(math.factorial(8), 39900))
+    print(f'rel error for p=8! and p*=39900:', rel_err(math.factorial(8), 39900))
+    print('')
